803462c08ffbf5ead46ad4ad2bcc53347af444e9
keym3n <nightfast@protonmail.com>
2025-02-02 14:15:08 +0300

    Update anvil.lua

--- anvil.lua
+++ anvil.lua
@@ -1,21 +1,3 @@
---[[
-	Anvil - a simple crafting station mod.
-	Copyright (C) 2019  Skamiz Kazzarch
-
-	This library is free software; you can redistribute it and/or
-	modify it under the terms of the GNU Lesser General Public
-	License as published by the Free Software Foundation; either
-	version 2.1 of the License, or (at your option) any later version.
-
-	This library is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-	Lesser General Public License for more details.
-
-	You should have received a copy of the GNU Lesser General Public
-	License along with this library; if not, write to the Free Software
-	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-]]
 local modname = minetest.get_current_modname()
 local modpath = minetest.get_modpath(modname)
 local S = minetest.get_translator(modname)
